556b1276550ce129bf6431adc5ccfc1d293487ba
iliya faramarzi <iliyafaramarzi1384@gmail.com>
2023-08-05 21:31:25 +0330

    create classes and added math captcha

--- main.py
+++ main.py
@@ -1,36 +1,54 @@
 import random
-from typing import Counter 
+from sympy.solvers import solve
+from sympy import Symbol
 
-def random_day():
-    a = random.randrange(1, 365)
-    return int(a)
+class date():
 
-def what_month(day):
-    days = [31,31,31,31,31,31,30,30,30,30,30,29]
-    count = 0
-    for i in days:
-        day -= i
-        if day <= 0 :
-            return count
-        else:
-            count += 1
+    def __init__(self) -> None:
+        pass
 
+    def random_day():
+        a = random.randrange(1, 365)
+        return int(a)
+
+    def what_month(day):
+        days = [31,31,31,31,31,31,30,30,30,30,30,29]
+        count = 0
+        for i in days:
+            day -= i
+            if day <= 0 :
+                return count
+            else:
+                count += 1
+
+    def what_month_word(month):
+        months = ['farvardin' , 'ordibehesht' , 'khordad' , 'tir' , 'mordad' , 'shahrivar' , 'mehr' , 'aban' , 'azar' , 'day' , 'bahman' , 'esfand']
+        return months[month]
+
+    def question_maker(month):
+        months = ['farvardin' , 'ordibehesht' , 'khordad' , 'tir' , 'mordad' , 'shahrivar' , 'mehr' , 'aban' , 'azar' , 'day' , 'bahman' , 'esfand']
+        final = []
+        final = random.choices(months, k=4)
+        if not month in final:
+            final[random.randrange(0, len(final))] = month
+        return final
+
+class math():
+
+    def __init__(self) -> None:
+        pass
+
+    def question_maker():
+        operations = ['+', '-', '/', '*']
+        return f'{random.randrange(0, 11)} {random.choice(operations)} {random.randrange(0, 11)}'
     
+    def answer(question):
+        x = Symbol('x')
+        return solve(f'{question} - x', x)
+    
+    def options(answer):
+        options = [answer]
+        for i in range(3):
+            options.append(math.answer(math.question_maker()))
 
-def what_month_word(month):
-    months = ['farvardin' , 'ordibehesht' , 'khordad' , 'tir' , 'mordad' , 'shahrivar' , 'mehr' , 'aban' , 'azar' , 'day' , 'bahman' , 'esfand']
-    return months[month]
-
-def question_maker(month):
-    months = ['farvardin' , 'ordibehesht' , 'khordad' , 'tir' , 'mordad' , 'shahrivar' , 'mehr' , 'aban' , 'azar' , 'day' , 'bahman' , 'esfand']
-    final = []
-    final = random.choices(months, k=4)
-    if not month in final:
-        final[random.randrange(0, len(final))] = month
-    return final
-
-
-
-test = random_day()
-print(test)
-print(question_maker(what_month_word(what_month(test))))
+        return options
